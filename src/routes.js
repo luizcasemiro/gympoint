21e448ccaf24963cab4a3e9ed5c26375643e8b84
a <a>
2020-01-22 16:21:23 -0300

    Auth on routes

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,13 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 routes.post('/students', (req, res) => res.json({ ok: true }));
 
